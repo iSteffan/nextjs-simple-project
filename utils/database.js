f8e5b3edfe9d824d6d3fa65e1e103d412ca5da1f
Stepan Skichko <st.skichko@gmail.com>
2024-01-10 13:08:06 +0200

    add use model  // add var for google & mongo

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('MongoBD is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'share_prompt',
+      useNewUrlParcer: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
