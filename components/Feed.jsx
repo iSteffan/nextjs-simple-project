413bbc73d712c8b0595faeb23aa4bb96178c9173
Stepan Skichko <st.skichko@gmail.com>
2024-01-11 16:39:53 +0200

    add promptCard

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,9 +6,9 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map(post => {
-        <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />;
-      })}
+      {data.map(post => (
+        <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+      ))}
     </div>
   );
 };
@@ -42,7 +42,7 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={[posts]} handleTagClick={() => {}} />
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
