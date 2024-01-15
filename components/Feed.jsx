570037a96dcd8aacda727898ce8fbe67c1027701
Stepan Skichko <st.skichko@gmail.com>
2024-01-15 11:59:09 +0200

    add search & search by tag click

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -15,8 +15,9 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
   const [posts, setPosts] = useState([]);
-  const handleSearchChange = e => {};
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -28,6 +29,32 @@ const Feed = () => {
 
     fetchPosts();
   }, []);
+
+  const filterPrompts = searchText => {
+    const regex = new RegExp(searchText, 'i'); // 'i' для пошуку без врахування регістру
+    return posts.filter(
+      item => regex.test(item.creator.username) || regex.test(item.tag) || regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = e => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = tagName => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className="feed">
@@ -42,7 +69,11 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList data={searchedResults} handleTagClick={handleTagClick} />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
